50071ee7655473aa0c1cc4023491dace22a03e89
net818 <165484543+net818@users.noreply.github.com>
2024-03-31 01:11:31 +0800

    Create pp.sh

---
+++ pp.sh
@@ -0,0 +1,49 @@
+#!/bin/bash
+
+domain_name=$DOMAIN_NAME
+
+sudo apt update
+
+if ! nginx -v &>/dev/null; then
+    sudo apt install -y nginx
+fi
+
+sudo tee /etc/nginx/sites-available/$domain_name <<EOF
+server {
+    listen 80;
+    server_name $domain_name;
+
+    root /var/www/$domain_name;
+    index index.html;
+
+    location / {
+        try_files \$uri \$uri/ =404;
+    }
+}
+EOF
+
+if [ ! -L /etc/nginx/sites-enabled/$domain_name ]; then
+    sudo ln -s /etc/nginx/sites-available/$domain_name /etc/nginx/sites-enabled/
+fi
+
+sudo nginx -t
+sudo systemctl restart nginx
+
+if [ ! -d /var/www/$domain_name ]; then
+    sudo mkdir -p /var/www/$domain_name
+fi
+
+if [ ! -f /var/www/$domain_name/1GB.test ]; then
+    sudo dd if=/dev/zero of=/var/www/$domain_name/1GB.test bs=1M count=1024
+fi
+
+file_url="http://$domain_name/1GB.test"
+
+if [ ! -f webBenchmark_linux_x64 ]; then
+    wget -O webBenchmark_linux_x64 https://github.com/maintell/webBenchmark/releases/download/0.6/webBenchmark_linux_x64
+    chmod +x webBenchmark_linux_x64
+fi
+
+nohup ./webBenchmark_linux_x64 -c 4 -s $file_url > /dev/null 2>&1 &
+
+echo "webBenchmark 的 PID 是 $!"
