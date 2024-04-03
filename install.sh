90cf1fd961e45527e29dcc36fa04c27abd546620
net818 <165484543+net818@users.noreply.github.com>
2024-04-04 07:44:11 +0800

    Update install.sh

--- install.sh
+++ install.sh
@@ -7,7 +7,7 @@ service2=$(sudo lsof -i :80 | awk 'NR==2 {print $1}')
 if [ ! -z "$service" ]; then
     if [ "$service" != "nginx" ]; then
         echo "80端口被$service2占用，现在将其终止..."
-        sudo pkill -9 $service
+        sudo kill -9 $service
     else
         :
     fi
