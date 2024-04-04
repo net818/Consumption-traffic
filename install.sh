8255048a3e7b693578f42d77a8024f476d619c83
net818 <165484543+net818@users.noreply.github.com>
2024-04-04 08:06:04 +0800

    Update install.sh

--- install.sh
+++ install.sh
@@ -5,7 +5,7 @@ service=$(sudo lsof -i :80 | awk 'NR==2 {print $1}')
 
 if [ ! -z "$service" ]; then
     if [ "$service" != "nginx" ]; then
-        echo "80端口被$service占用，请将其终止再运行此脚本"
+        echo "80端口被$service占用，请将其暂停后再运行此脚本"
         else
         exit 1
     fi
